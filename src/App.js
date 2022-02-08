1ea04daa1d79efdb9d2f1922d47bc15de6141190
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-02-08 08:38:46 -0500

    added inline styles to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 
-function App() {
+const styledText = {
+  fontSize: "25px",
+  textAlign: "center",
+  margin: "40px",
+  border: "1px solid green"
+};
+
+const imageStyle = {
+  borderRadius: "60%"
+};
+
+ export default function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div style={styledText}>
+      <h1> Hello World, we're practicing Inline Styling in React!</h1>
+      <img 
+      style={imageStyle}
+      src="https://placekitten.com/200/300"
+      alt="cute kitten"
+      />
     </div>
   );
 }
 
-export default App;
